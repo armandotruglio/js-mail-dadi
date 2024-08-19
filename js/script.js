ada732bb3fb36c6acaee7133401f1044204146c9
armandotruglio <armando.truglio1@gmail.com>
2024-08-19 17:31:17 +0200

    Finished dice game

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 console.log('JS OK');
+
+// DADI
+
+const firstDice = document.getElementById('first-dice');
+const secondDice = document.getElementById('second-dice');
+const winnerDice = document.getElementById('winner');
+
+const min = 1;
+const max = 6;
+
+
+const firstThrow = Math.floor(Math.random() * (max - min + 1) + min);
+const secondThrow = Math.floor(Math.random() * (max - min + 1) + min);
+
+
+firstDice.innerHTML = firstThrow;
+secondDice.innerHTML = secondThrow;
+
+console.log(firstThrow);
+console.log(secondThrow);
+
+if(firstThrow > secondThrow){
+    winnerDice.innerHTML = 'Vince il primo dado';
+}
+else if(firstThrow < secondThrow){
+    winnerDice.innerHTML = 'Vince il secondo dado';
+}
+else{
+    winnerDice.innerHTML = 'I dadi hanno pareggiato';
+}
